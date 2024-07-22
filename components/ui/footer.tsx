c89f5660d1a0b94700c17aac49e797bb5aca2936
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-07-22 23:14:39 +0800

    feat: fix footer responsivity

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Copy, Instagram, Twitter, Youtube } from "lucide-react"
 import Image from "next/image"
 
@@ -15,16 +16,16 @@ const Footer: React.FC<FooterProps> = ({
                 alt="Footer Background"
                 width={1920}
                 height={412}
-                className="w-full absolute bottom-[412px] object-top object-cover z-[-1] hidden md:flex"
+                className="w-full absolute bottom-[412px] sm:bottom-[262px] md:bottom-[412px] object-top object-cover z-[-1] hidden sm:flex"
             />
             <Image
                 src={'/footer-mobile.png'}
                 alt="Footer Background"
                 width={1920}
                 height={412}
-                className="w-full absolute bottom-[262px] h-[450px] object-top object-cover z-[-1] md:hidden"
+                className="w-full absolute bottom-[262px] object-top object-cover z-[-1] sm:hidden"
             />
-            <section className="z-20 flex flex-col gap-4 bg-gradient-to-b from-[#343FC0] to-[#080B2D] h-[262px] md:h-[412px] p-5 md:p-10 w-full">
+            <section className="z-20 flex flex-col gap-4 bg-[#343FC0] h-[262px] md:h-[412px] p-5 md:p-10 w-full">
                 <div className="flex items-center justify-center text-white font-bold text-sm font-tex-gyre gap-2 md:hidden">
                     <h5 className="">Supported by</h5>
                     <Image
@@ -34,49 +35,64 @@ const Footer: React.FC<FooterProps> = ({
                         height={120}
                     />
                 </div>
-                <div className="flex items-center justify-between w-full h-fit">
+                <div className="flex items-center justify-between w-full h-fit gap-4">
                     <div className="flex gap-12">
                         <Image
-                            src={'/oh-logo.svg'}
+                            src={'/oh-logo-text-white.png'}
                             alt="Open House Logo"
                             width={251}
                             height={303.23}
                             className="w-[124.8px] h-[156px] md:w-[251px] md:h-[303.23px]"
                         />
-                        <div className="hidden lg:flex flex-col justify-center gap-8 font-tex-gyre text-white font-bold text-xl">
+                        <div className="hidden lg:flex flex-col justify-center gap-8 font-tex-gyre text-Text/TextDarkBG font-bold text-base">
                             {
                                 ["Ambassador", "Dashboard", "Registration Page", "Merchandise", "Wall of Fame"].map((item) => {
                                     return (
-                                        <li className="list-none">
+                                        <a
+                                            href={item}
+                                            className="list-none hover:text-RedRegion/Monza/700 hover:scale-105 duration-200 cursor-pointer">
                                             {item}
-                                        </li>
+                                        </a>
                                     )
                                 })
                             }
                         </div>
                     </div>
-                    <div className="flex gap-32 font-tex-gyre font-bold text-white justify-end">
+                    <div className="flex gap-10 lg:gap-16 xl:gap-32 font-tex-gyre font-bold text-white justify-end">
                         <div className="flex flex-col gap-4 md:gap-8 text-xl ">
                             <div className="flex flex-col items-end md:items-start gap-4">
-                                <h5 className="text-right md:text-left text-xs md:text-sm">Find Us on Social Media</h5>
-                                <div className="flex gap-3 lg:gap-9 items-center">
-                                    <Instagram className="w-4 ld:w-6 aspect-square"/>
-                                    <Twitter className="fill-white w-4 lg:w-6"/>
-                                    <Youtube className="w-4 lg:w-6" />
-                                    <Image
-                                        src={'/tiktok.svg'}
-                                        alt="Tiktok Logo"
-                                        width={24}
-                                        height={24}
-                                        className="w-3 lg:w-4"
-                                    />
+                                <h5 className="text-right md:text-left text-xs md:text-xl">Find Us on Social Media</h5>
+                                <div className="flex gap-3 lg:gap-9 items-center cursor-pointer">
+                                    <a href="">
+                                        <Instagram className="w-4 lg:w-8 aspect-square cursor-pointer"/>
+                                    </a>
+                                    <a href="">
+                                        <Twitter className="fill-white w-4 lg:w-8 cursor-pointer"/>
+                                    </a>
+                                    <a href="">
+                                        <Youtube className="w-4 lg:w-10 cursor-pointer" fill="white"/>
+                                    </a>
+                                    <a href="">
+                                        <Image
+                                            src={'/tiktok.svg'}
+                                            alt="Tiktok Logo"
+                                            width={24}
+                                            height={24}
+                                            className="min-w-3 w-3 lg:min-w-4 lg:w-4 cursor-pointer"
+                                        />
+                                    </a>
                                 </div>
                             </div>
                             <div className="flex flex-col items-end md:items-start">
-                                <h5 className="text-xs md:text-sm">Contact Us</h5>
+                                <h5 className="text-lg md:text-xl">Contact Us</h5>
                                 <div className="flex gap-2 items-center">
-                                    <span className="font-normal text-xs flex:text-sm">openhouse@cs.ui.ac.id</span>
-                                    <Copy className="w-3 md:w-4 aspect-square cursor-pointer hover:scale-110 duration-100"/>
+                                    <span className="font-normal text-xs flex md:text-base">openhouse@cs.ui.ac.id</span>
+                                    <Copy
+                                        onClick={() => {
+                                            // Copy to clipboard
+                                            navigator.clipboard.writeText("openhouse@cs.ui.ac.id")
+                                        }}
+                                        className="w-3 md:w-4 aspect-square cursor-pointer hover:scale-110 duration-100" />
                                 </div>
                             </div>
                             <h3 className="text-xl text-right md:hidden">#MoreThanIT</h3>

fa73d3ed5f3e6ca0c21d62e48fb2c61c87fcad11
secona <77039267+secona@users.noreply.github.com>
2024-07-26 09:46:33 +0700

    feat: accordion component

---
+++ components/ui/Accordion.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta } from '@storybook/react'
+import Button from './Button'
+import Accordion from './Accordion'
+
+const meta: Meta<typeof Button> = {
+  title: 'Components/Accordion',
+}
+
+export default meta
+
+export const Main = () => (
+  <Accordion type="single" collapsible>
+    <Accordion.Item value="item-1">
+      <Accordion.Trigger>Is it accessible?</Accordion.Trigger>
+      <Accordion.Content>
+        Yes. It adheres to the WAI-ARIA design pattern.
+      </Accordion.Content>
+    </Accordion.Item>
+    <Accordion.Item value="item-2">
+      <Accordion.Trigger>Is it accessible?</Accordion.Trigger>
+      <Accordion.Content>
+        Yes. It adheres to the WAI-ARIA design pattern.
+      </Accordion.Content>
+    </Accordion.Item>
+    <Accordion.Item value="item-3">
+      <Accordion.Trigger>Is it accessible?</Accordion.Trigger>
+      <Accordion.Content>
+        Yes. It adheres to the WAI-ARIA design pattern.
+      </Accordion.Content>
+    </Accordion.Item>
+  </Accordion>
+)

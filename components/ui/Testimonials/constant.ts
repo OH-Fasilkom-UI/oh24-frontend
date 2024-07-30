9694c03d67e21f1ed3f4d77303ebe4d3e747da53
gracekarinn <gracekarina12@gmail.com>
2024-07-30 10:12:05 +0700

    feat: add Testimonials component

---
+++ components/ui/Testimonials/constant.ts
@@ -0,0 +1,44 @@
+export const testimonials = [
+    {
+        name: 'Andrew',
+        foto: '/contoh-testi.png',
+        testimonial:
+        '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        namaDanSekolah: 'Andrew, SMAN 1 Sumbawa Besar',
+    },
+    {
+        name: 'Aileen',
+        testimonial:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id semper tortor. Nullam nec lectus ut massa ultricies tincidunt. Donec at odio nec purus tincidunt ultricies. Nullam id semper tortor. Nullam nec lectus ut massa ultricies tincidunt.',
+        foto: '/contoh-testi2.png',
+        namaDanSekolah: 'Andrew, SMAN 1 Sumbawa Besar',
+    },
+    {
+        name: 'Muttaqin',
+        testimonial:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id semper tortor. Nullam nec lectus ut massa ultricies tincidunt. Donec at odio nec purus tincidunt ultricies. Nullam id semper tortor. Nullam nec lectus ut massa ultricies tincidunt.',
+        foto: '/contoh-testi.png',
+        namaDanSekolah: 'Andrew, SMAN 1 Sumbawa Besar',
+    },
+    {
+        name: 'Joanne',
+        testimonial:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id semper tortor. Nullam nec lectus ut massa ultricies tincidunt. Donec at odio nec purus tincidunt ultricies. Nullam id semper tortor. Nullam nec lectus ut massa ultricies tincidunt.',
+        foto: '/contoh-testi2.png',
+        namaDanSekolah: 'Andrew, SMAN 1 Sumbawa Besar',
+    },
+    {
+        name: 'Radit',
+        testimonial:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id semper tortor. Nullam nec lectus ut massa ultricies tincidunt. Donec at odio nec purus tincidunt ultricies. Nullam id semper tortor. Nullam nec lectus ut massa ultricies tincidunt.',
+        foto: '/contoh-testi.png',
+        namaDanSekolah: 'Andrew, SMAN 1 Sumbawa Besar',
+    },
+    {
+        name: 'Valen',
+        testimonial:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id semper tortor. Nullam nec lectus ut massa ultricies tincidunt. Donec at odio nec purus tincidunt ultricies. Nullam id semper tortor. Nullam nec lectus ut massa ultricies tincidunt.',
+        foto: '/contoh-testi2.png',
+        namaDanSekolah: 'Andrew, SMAN 1 Sumbawa Besar',
+    },
+]   

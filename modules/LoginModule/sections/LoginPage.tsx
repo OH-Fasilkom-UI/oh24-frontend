c3755d2b21bf0720a8be1ac544078cc4c233fd9e
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-08-01 22:49:43 +0800

    feat: fix responsivity in login and profile page

--- modules/LoginModule/sections/LoginPage.tsx
+++ modules/LoginModule/sections/LoginPage.tsx
@@ -9,7 +9,7 @@ export const LoginPage = () => {
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.3, type: 'spring', stiffness: 260, damping: 20 }}
-      className="flex flex-col items-center">
+      className="flex flex-col items-center mt-20 md:mt-32">
       <div
         className="font-bold flex flex-col md:gap-8 tracking-[1.5px] leading-[36px] font-riffic text-center md:text-[50px] lg:text-[60px] max-md:text-[30px] text-[#2E3881] pt-[64px]">
         <p>Welcome to </p>

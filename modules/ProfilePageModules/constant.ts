087e0c8eee63ceb78bc215ed0b82cfbf8118d615
Tristan A. <tristanagraa@gmail.com>
2024-08-01 16:59:10 +0700

    feat: add profile constant image

---
+++ modules/ProfilePageModules/constant.ts
@@ -0,0 +1,53 @@
+export const DetailProfileConstant = [
+    {
+      title: 'Nama Lengkap',
+      value: 'Nak Oha',
+    },
+    {
+      title: 'Email',
+      value: "NakOha@gmail.com",
+    },
+    {
+      title: 'Domisili',
+      value: 'Jabodetabek',
+    },
+    {
+      title: 'WhatsApp',
+      value: "086969696969696",
+    },
+    {
+      title: 'Tanggal Lahir',
+      value: '19/02/2005',
+    },
+    {
+      title: 'Kelas',
+      value: "Gaypear",
+    },
+    {
+      title: 'Asal Sekolah',
+      value: 'SMAN 1 Bangka Belitung',
+    },
+    {
+      title: 'Nomor Telepon Orang Tua',
+      value: "089696969696969",
+    },
+  ]
+
+export const PandaImages = [
+  {
+    alt: 'Panda Cowo 1',
+    src: '/Profile/cowo1.png',
+  },
+  {
+    alt: 'Panda Cowo 2',
+    src: '/Profile/cowo2.png',
+  },
+  {
+    alt: 'Panda Cewe 1',
+    src: '/Profile/cewe1.png',
+  },
+  {
+    alt: 'Panda Cewe 2',
+    src: '/Profile/cewe2.png',
+  }
+]

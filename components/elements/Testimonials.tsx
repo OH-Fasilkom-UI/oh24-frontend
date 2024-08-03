8722aada0935cc1fcbea2c61fd006b581b162a6e
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-08-03 11:47:34 +0800

    feat: fix responsivity of testimonials, add optional testimony data

--- components/elements/Testimonials.tsx
+++ components/elements/Testimonials.tsx
@@ -6,7 +6,11 @@ import { ScrollArea, ScrollBar } from '@/components/ui/ScrollArea'
 import { Avatar, AvatarImage } from '@radix-ui/react-avatar'
 import { useState } from 'react'
 
-const Testimonials = () => {
+const Testimonials = ({
+  testimonyData = testimonials,
+}: {
+  testimonyData?: typeof testimonials
+}) => {
   const [active, setActive] = useState(0)
 
   return (
@@ -18,12 +22,12 @@ const Testimonials = () => {
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
         }}
-        className="flex flex-col items-center rounded-xl lg:h-fit lg:w-full md:w-fit sm:h-[498px] sm:w-[389px]"
+        className="flex flex-col items-center rounded-xl w-full"
       >
-        <div className="flex lg:gap-[60px] md:gap-[30px] md:px-[30px] md:w-fit md:h-fit max-md:gap-[20px] max-md:items-center max-md:flex-col justify-center flex-row lg:px-[40px] max-md:py-[20px] py-[64px] items-start">
+        <div className="flex lg:gap-[60px] md:gap-[30px] px-[30px] w-full md:h-fit max-md:gap-[20px] max-md:items-center max-md:flex-col justify-center flex-row lg:px-[40px] max-md:py-8 py-[64px] items-start">
           <Avatar>
             <AvatarImage
-              src={testimonials[active].foto}
+              src={(testimonyData ?? testimonials)[active].foto}
               alt="quote"
               width={180}
               height={180}
@@ -31,26 +35,26 @@ const Testimonials = () => {
             />
           </Avatar>
           <div className="flex gap-[24px] md:gap-[20px] flex-col lg:w-[780px] md:w-[530px] max-sm:w-fit max-sm:px-[10px] max-md:w-[283px]">
-            <p className="text-Text/TextDarkBG font-tex-gyre max-md:text-[14px] leading-9 max-md:leading-[25px] font-extrabold text-justify md:text-[18px] text-[24px]">
-              {testimonials[active].testimonial}
+            <p className="text-Text/TextDarkBG font-tex-gyre max-md:text-[14px] leading-9 max-md:leading-[25px] font-extrabold text-justify md:text-[18px] text-[24px] max-sm:max-h-[250px] overflow-y-scroll sm:overflow-visible">
+              {(testimonyData ?? testimonials)[active].testimonial}
             </p>
             <p className="text-Text/TextDarkBG font-tex-gyre max-md:text-[14px] md:text-[20px] font-normal text-[24px]">
-              {testimonials[active].namaDanSekolah}
+              {(testimonyData ?? testimonials)[active].namaDanSekolah}
             </p>
           </div>
         </div>
       </div>
       <div>
         <ScrollArea>
-          <div className="flex flex-row max-sm:gap-[10px] gap-[60px] justify-center py-[64px]">
-            {testimonials.map((testi, index) => (
+          <div className="flex flex-row max-sm:gap-[10px] gap-[40px] justify-center py-10">
+            {(testimonyData ?? testimonials).map((testi, index) => (
               <div
                 onClick={() => setActive(index)}
                 key={index}
-                className="flex flex-col justify-center items-center gap-[24px]"
+                className="flex flex-col justify-center items-center gap-[24px] hover:scale-105 duration-300 cursor-pointer"
               >
                 <div
-                  className={`flex pt-6 rounded-2xl items-center flex-col ${active === index ? 'bg-Text/TextLightBG' : 'bg-[#1C274F]'} text-center gap-[24px] max-md:gap-[8px] max-md:w-[120px] max-md:h-[132px] max-md:pt-[12px] w-[190px] h-[236px]`}
+                  className={`flex pt-6 rounded-2xl items-center flex-col ${active === index ? 'bg-Text/TextLightBG' : 'bg-[#1C274F]'} text-center gap-2 max-md:w-[120px] max-md:h-[132px] max-md:pt-[12px] w-[190px] h-[236px]`}
                 >
                   <Avatar>
                     <AvatarImage
@@ -61,7 +65,7 @@ const Testimonials = () => {
                       className="max-md:w-[80px] max-md:h-[80px]"
                     />
                   </Avatar>
-                  <p className="text-Text/TextDarkBG font-tex-gyre font-normal max-md:text-[14px] md:text-[20px] text-[24px]">
+                  <p className="text-Text/TextDarkBG font-tex-gyre font-bold max-md:text-[14px] md:text-[20px] text-[24px]">
                     {testi.name}
                   </p>
                 </div>

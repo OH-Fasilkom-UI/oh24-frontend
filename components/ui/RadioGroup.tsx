e555f7438f0d418de075d65965b0efd71df1ce7d
secona <77039267+secona@users.noreply.github.com>
2024-08-04 10:18:08 +0700

    feat: use new schema in DetailForm

--- components/ui/RadioGroup.tsx
+++ components/ui/RadioGroup.tsx
@@ -80,4 +80,4 @@ const RadioGroup = React.forwardRef<
 RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
 RadioGroup.Item = RadioGroupItem
 
-export { RadioGroup, RadioGroupItem }
+export default RadioGroup
--- modules/DetailFormModule/sections/DetailForm.tsx
+++ modules/DetailFormModule/sections/DetailForm.tsx
@@ -3,62 +3,25 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import * as z from 'zod'
 import Card from '@/components/ui/Card'
 import { Form } from '@/components/ui/form'
 import InputForm from '../components/InputForm'
 import Button from '@/components/ui/Button'
-import { RadioGroup, RadioGroupItem } from '@/components/ui/RadioGroup'
+import RadioGroup from '@/components/ui/RadioGroup'
 import { Label } from '@/components/ui/label'
-
-export const formSchema = z.object({
-  namaLengkap: z.string().min(3, 'Nama Lengkap harus diisi'),
-  domisili: z.enum(['Jabodetabek', 'Luar Jabodetabek']),
-  tanggalLahir: z
-    .string()
-    .regex(
-      /^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[012])-\d{4}$/,
-      'Format harus DD-MM-YYYY'
-    ),
-  asalSekolah: z.string().min(5, 'Asal Sekolah hsarus diisi'),
-  nomorWhatsApp: z
-    .string()
-    .regex(
-      /^(\+62|62|0)8[1-9][0-9]{8,10}$/,
-      'Nomor harus berawalan 08 dan minimal 10 digit'
-    ),
-  kelas: z.enum([
-    'Kelas 10',
-    'Kelas 11',
-    'Kelas 12',
-    'Gap Year/Semi Gap',
-    'SMP',
-  ]),
-  namaOrangtua: z.string().min(5, 'Nama Orangtua harus diisi'),
-  nomorTeleponOrangtua: z
-    .string()
-    .regex(
-      /^(\+62|62|0)8[1-9][0-9]{8,10}$/,
-      'Nomor harus berawalan 08 dan minimal 10 digit'
-    ),
-})
+import {
+  Class,
+  Domicile,
+  SubmitPersonalData,
+  submitPersonalDataSchema,
+} from '@/lib/api/registration'
 
 const DetailForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      namaLengkap: '',
-      domisili: 'Jabodetabek',
-      tanggalLahir: '',
-      asalSekolah: '',
-      nomorWhatsApp: '',
-      kelas: 'Kelas 10',
-      namaOrangtua: '',
-      nomorTeleponOrangtua: '',
-    },
+  const form = useForm<SubmitPersonalData>({
+    resolver: zodResolver(submitPersonalDataSchema),
   })
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: SubmitPersonalData) {
     // nanti diubah jadi logic submit
     console.log(values)
   }
@@ -74,7 +37,7 @@ const DetailForm = () => {
             <InputForm
               label="Nama Lengkap"
               placeholder="Nama lengkap harus diisi"
-              name="namaLengkap"
+              name="fullName"
               form={form}
             />
 
@@ -85,47 +48,38 @@ const DetailForm = () => {
               <RadioGroup
                 size="normal"
                 color="light"
-                onValueChange={(value) =>
-                  form.setValue(
-                    'domisili',
-                    value as 'Jabodetabek' | 'Luar Jabodetabek'
-                  )
+                onValueChange={(value: unknown) =>
+                  form.setValue('domicile', value as Domicile)
                 }
-                defaultValue={form.getValues('domisili')}
                 className="flex flex-col space-y-1"
               >
-                <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="Jabodetabek" id="jabodetabek" />
-                  <label htmlFor="jabodetabek">Jabodetabek</label>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <RadioGroupItem
-                    value="Luar Jabodetabek"
-                    id="luar-jabodetabek"
-                  />
-                  <Label htmlFor="luar-jabodetabek">Luar Jabodetabek</Label>
-                </div>
+                <RadioGroup.Item value="JABODETABEK" id="jabodetabek">
+                  Jabodetabek
+                </RadioGroup.Item>
+                <RadioGroup.Item value="NON_JABODETABEK" id="luar-jabodetabek">
+                  Luar Jabodetabek
+                </RadioGroup.Item>
               </RadioGroup>
             </div>
 
             <InputForm
               label="Tanggal Lahir"
               placeholder="DD-MM-YYYY"
-              name="tanggalLahir"
+              name="dob"
               form={form}
             />
 
             <InputForm
               label="Asal Sekolah"
               placeholder="Jika Gap Year/Semi Gap Year, maka isi '-'"
-              name="asalSekolah"
+              name="school"
               form={form}
             />
 
             <InputForm
               label="Nomor WhatsApp"
               placeholder="Contoh: 08123456789"
-              name="nomorWhatsApp"
+              name="phone"
               form={form}
             />
           </div>
@@ -138,43 +92,46 @@ const DetailForm = () => {
               <RadioGroup
                 size="normal"
                 color="light"
-                onValueChange={(value) =>
-                  form.setValue(
-                    'kelas',
-                    value as z.infer<typeof formSchema>['kelas']
-                  )
+                onValueChange={(value: unknown) =>
+                  form.setValue('class', value as Class)
                 }
-                defaultValue={form.getValues('kelas')}
                 className="flex flex-col space-y-1"
               >
-                {[
-                  'Kelas 10',
-                  'Kelas 11',
-                  'Kelas 12',
-                  'Gap Year/Semi Gap',
-                  'SMP',
-                ].map((kelas) => (
-                  <div key={kelas} className="flex items-center space-x-2">
-                    <RadioGroupItem value={kelas} id={kelas} />
-                    <label htmlFor={kelas}>{kelas}</label>
-                  </div>
-                ))}
+                <RadioGroup.Item value="SMA_KELAS_10_SEDERAJAT" id="kelas-10">
+                  Kelas 10
+                </RadioGroup.Item>
+                <RadioGroup.Item value="SMA_KELAS_11_SEDERAJAT" id="kelas-11">
+                  Kelas 11
+                </RadioGroup.Item>
+                <RadioGroup.Item value="SMA_KELAS_12_SEDERAJAT" id="kelas-12">
+                  Kelas 12
+                </RadioGroup.Item>
+                <RadioGroup.Item value="GAPYEAR" id="gapyear">
+                  Gapyear
+                </RadioGroup.Item>
+                <RadioGroup.Item value="SEMIGAP" id="semigap">
+                  Semigap
+                </RadioGroup.Item>
+                <RadioGroup.Item value="SMP_SEDERAJAT" id="smp">
+                  SMP
+                </RadioGroup.Item>
               </RadioGroup>
             </div>
 
             <InputForm
               label="Nama Orang tua"
               placeholder="Nama orang tua harus diisi"
-              name="namaOrangtua"
+              name="parentName"
               form={form}
             />
 
             <InputForm
               label="Nomor Telepon Orang Tua"
               placeholder="Contoh: 08123456789"
-              name="nomorTeleponOrangtua"
+              name="parentPhone"
               form={form}
             />
+
             <div className=" flex justify-start w-full pt-3">
               <Button type="submit" className="w-full lg:w-auto">
                 Sign Up

1500f30bd2f9c0ada43c90a8fac9ca8b67e99673
gracekarinn <gracekarina12@gmail.com>
2024-08-09 14:39:48 +0700

    feat: add ambassador carousel (unfinished)

---
+++ modules/AmbassadorModule/components/AmbassadorCarousel.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React from 'react'
+
+const AmbassadorCarousel = () => {
+  return (
+    <div className="py-20">
+      <div className="mx-auto px-4">
+        <h1 className="text-center font-bold text-4xl tracking-widest text-Text/TextLightBG mb-4">
+          Ambassador Last Year
+        </h1>
+        <p className="text-center font-bold text-Text/TextLightBG mb-8 max-w-2xl mx-auto">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default AmbassadorCarousel

7db50b3f6358b6dfea62a72408f6a6992755d143
gracekarinn <gracekarina12@gmail.com>
2024-08-10 16:04:09 +0700

    feat: add constant

--- modules/AmbassadorModule/constant.ts
+++ modules/AmbassadorModule/constant.ts
@@ -25,3 +25,36 @@ export const LAST_AMBASSADOR = [
     description: 'Deskripsi Lorem ipsum dolor sit amet',
   },
 ]
+
+export const AMBASSADOR_PROGRAMS = [
+  {
+    id: 1,
+    title: '1. Welcoming Ambassador',
+    description:
+      'Welcoming OH Ambassador merupakan sebuah inisiatif yang dirancang untuk memfasilitasi pertemuan dan interaksi antar OH Ambassador dan staf yang bertujuan untuk memperkenalkan mereka secara lebih dalam tentang kegiatan-kegiatan yang terdapat di Open House Fasilkom UI maupun keuntungan yang akan didapatkan para OH Ambassador.',
+  },
+  {
+    id: 2,
+    title: '2. Webinar with Alumni',
+    description:
+      'OH Ambassador akan mendapatkan kesempatan untuk berpartisipasi dalam webinar mengenai aplikasi IT dan prospek kerja yang akan dibawakan oleh alumni Fasilkom UI yang telah sukses dalam bidang IT. Melalui webinar ini, para OH Ambassador akan mendapatkan wawasan dan pengetahuan berharga langsung dari ahli industri, agar update dengan tren dan perkembangan terbaru di dunia teknologi.',
+  },
+  {
+    id: 3,
+    title: '3. Ristek Class',
+    description:
+      'Para OH Ambassador akan mendapatkan akses eksklusif ke kelas RISTEK yang dirancang untuk meningkatkan keterampilan teknis dan menambah pemahaman lebih dalam tentang topik IT yang dibawakan oleh narasumber. RISTEK Class akan memberikan OH Ambassador pengalaman hands-on mengenai ilmu yang dapat diaplikasikan di dunia nyata.',
+  },
+  {
+    id: 4,
+    title: '4. 1-on-1 Mentoring',
+    description:
+      'One-on-one mentoring memberikan kesempatan kepada para OH Ambassador untuk dapat berinteraksi secara 1-on-1 dengan mentor, saling berdiskusi, dan mendapatkan informasi mengenai Fakultas Ilmu Komputer UI secara eksklusif. Selain itu, sesi ini juga melatih kemampuan komunikasi dan kepercayaan diri Ambassador.',
+  },
+  {
+    id: 5,
+    title: '5. Farewell Party',
+    description:
+      'Kegiatan penghujung acara yang akan dilaksanakan untuk para OH Ambassador yang telah aktif mempromosikan OH Fasilkom. Kegiatan ini bertujuan untuk menimbulkan kesan akhir yang baik bagi para OH Ambassador yang telah turut serta dalam rangkaian acara OH Fasilkom. Melalui Farewell party, diharapkan para OH Ambassador dapat merasakan apresiasi dalam menyukseskan acara OH Fasilkom itu sendiri.',
+  },
+]

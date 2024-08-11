668d0a4b453b65671e20ee49e1fb5bacdac770ed
Tristan A. <tristanagraa@gmail.com>
2024-08-11 08:36:01 +0700

    feat: label styling for form

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -95,7 +95,11 @@ const FormLabel = React.forwardRef<
   return (
     <Label
       ref={ref}
-      className={cn(error && 'text-destructive', className)}
+      className={cn(
+        error && 'text-destructive',
+        className,
+        'font-tex-gyre text-[16px] leadign-6 font-bold text-Text/TextDarkBG'
+      )}
       htmlFor={formItemId}
       {...props}
     />

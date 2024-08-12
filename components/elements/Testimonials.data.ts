ec1941b3d6b73006218ae2c05b41162de3a99965
gracekarinn <gracekarina12@gmail.com>
2024-08-12 15:23:36 +0700

    feat: change to copywriter data

--- components/elements/Testimonials.data.ts
+++ components/elements/Testimonials.data.ts
@@ -1,44 +1,44 @@
 export const testimonials = [
   {
-    name: 'Andrew',
-    foto: '/contoh-testi.png',
+    name: 'Naomyscha Attalie Maza',
+    foto: '/naomyscha.jpg',
     testimonial:
-      '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    namaDanSekolah: 'Andrew, SMAN 1 Sumbawa Besar',
+      '“Menjadi Ambassador Open House Fasilkom UI tahun lalu was an incredibly valuable experience! Selain mendapatkan banyak pengalaman dan skill baru, aku juga berkesempatan mengikuti hands-on session yang membantu aku memahami lebih dalam tentang Fasilkom UI. Insight dari para alumni juga sangat informatif, ditambah dengan sesi mentoring 1-on-1 yang memungkinkan aku untuk bertanya secara langsung tentang Fasilkom UI kepada kakak-kakak. Aku pun bisa networking with temen-temen yang juga berminat untuk masuk ke Fasilkom and share my enthusiasm with them! Participating as one of the ambassadors was absolutely worth it karena aku mendapat begitu banyak insights yang membuatku jauh lebih percaya diri untuk dapat masuk Fasilkom. Thankfully, sekarang aku sudah berhasil menjadi salah satu mahasiswa baru Fasilkom UI, dan pengalaman sebagai ambassador tahun lalu definitely menjadi salah satu faktor penentu in my journey!”',
+    namaDanSekolah: 'Naomyscha, SMA Labschool Kebayoran',
   },
   {
-    name: 'Aileen',
+    name: 'Muhammad Helmi Alfarissi',
     testimonial:
-      '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    foto: '/contoh-testi2.png',
-    namaDanSekolah: 'Andrew, SMAN 1 Sumbawa Besar',
+      '“Haloo teman-teman, aku Helmi, di sini aku mau sharing sedikit experienceku sebagai Ambassador OH Fasilkom di tahun 2023. My experience being an OH Ambassador adalah salah satu pengalaman yang paling berharga dan enlighting banget. As camaba yang punya mimpi jadi bagian dari Fasilkom UI di jurusan Sistem Informasi pengalaman ini memberikan aku banyak insight baru tentang Fasilkom dan dunia perkuliahan. Di mana as Ambassador kita difasilitasi banyak program yang totally beneficial dan seru pastinya. Mulai dari Welcoming Ambassador, di mana kita mendapatkan penjelasan lebih lanjut tentang tugas sebagai OH Ambassador dan Fasilkom in general. Selain itu, banyak juga program seru lainnya seperti Webinar with Alumni, RISTEK Class, 1-on-1 Mentoring Session, dan masih banyak lagi kegiatan seru lainnya. Menjadi Ambassador memberikan aku pemahaman lebih lanjut tentang dunia IT, mulai dari basicnya seperti materi Computational Thinking sampai pembelajaran skill yang diajarkan langsung oleh RISTEK Fasilkom, yaitu organisasi bergengsi yang ada di Fasilkom UI. Being an Ambassador juga memberikan aku pacuan motivasi untuk terus memperjuangkan mimpiku demi menjadi bagian dari Sistem Informasi Fasilkom UI. So, tunggu apalagi guys, yuk tambah pengalaman kalian dan lebih tau lagi tentang Fasilkom UI dengan menjadi OH Ambassador tahun 2024 🤩”',
+    foto: '/helmi.jpg',
+    namaDanSekolah: 'Helmi, SMAN 59 Jakarta',
   },
   {
-    name: 'Muttaqin',
+    name: 'Nalakrishna Abimanyu Wicaksono',
     testimonial:
-      '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    foto: '/contoh-testi.png',
-    namaDanSekolah: 'Andrew, SMAN 1 Sumbawa Besar',
+      '“ Jujur awalnya aku kira jadi ambassador cuma kaya promosiin acara OH ini dengan bikin video atau post story. ternyata aku salah. Dengan jadi ambassador, aku dapet banyak banget hal selain itu. Aku dapet insight yang bagus banget tentang RISTEK yang bahkan aku belum pernah denger sebelumnya. Selain itu, aku jadi tau gambaran kuliah di pacil gimana dari sesi mentoring 1 on 1 bersama kakak” dari pacil. Intinya I would recommend you buat jadi ambassador karena emang bermanfaat banget buat kalian yang tertarik masuk pacil”',
+    foto: '/nalakrishna.jpg',
+    namaDanSekolah: 'Nalakrishna, SMA Labschool Kebayoran',
   },
   {
-    name: 'Joanne',
+    name: 'Azellea Sariyanto',
     testimonial:
-      '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    foto: '/contoh-testi2.png',
-    namaDanSekolah: 'Andrew, SMAN 1 Sumbawa Besar',
+      '“Menjadi bagian dari OH Fasilkom UI Student Ambassador 2023 merupakan salah satu pengalaman hidupku yang paling berkesan. Gimana enggak? Selama menjadi Student Ambassador aku belajar banyak hal mulai dari pembuatan konten, digital branding, dan bahkan insight perihal Fasilkom UI itu sendiri. Hal ini benar-benar mengembangkan skill diriku dalam berorganisasi dan memperluas pengetahuan serta relasiku. Aku sangat berterima kasih untuk semua kakak-kakak Pacil yang sudah membimbing aku. Main Event pun meninggalkan kesan berharga bagiku karena di sana aku dan teman-teman Student Ambassador lainnya bisa berkumpul secara langsung. Gak hanya itu, saat acara berlangsung kami juga diberi apresiasi oleh panitia OH Fasilkom UI atas kontribusi kami selama menjalani program Student Ambassador. Pokoknya, kalian gak akan nyesel deh kalau join Student Ambassador OH Fasilkom UI~!”',
+    foto: '/azellea.jpg',
+    namaDanSekolah: 'Azellea, SMKN 42 Jakarta',
   },
   {
-    name: 'Radit',
+    name: 'Apriansyah',
     testimonial:
-      '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    foto: '/contoh-testi.png',
-    namaDanSekolah: 'Andrew, SMAN 1 Sumbawa Besar',
+      '“Being a student ambassador for OH Fasilkom has been an enriching experience. Aku dapat meningkatkan kepercayaan diri, melatih time management, mendapatkan banyak relasi, dan dapat mempelajari banyak hal baru tentang kegiatan OH Fasilkom ini. Selain itu, menjadi Student Ambassador juga dapat membantu aku dalam membangun personal branding and it can provide a valuable new experience."',
+    foto: '/apriansyah.jpg',
+    namaDanSekolah: 'Apriansyah, SMAN 1 Kabandungan',
   },
   {
-    name: 'Valen',
+    name: 'Naila Salsabilla',
     testimonial:
-      '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    foto: '/contoh-testi2.png',
-    namaDanSekolah: 'Andrew, SMAN 1 Sumbawa Besar',
+      '“Being part of student ambassadors OH Fasilkom UI was truly an unforgettable high school experience!! bener bener seneng bangett bisa jadi bagian dari OH Fasilkom UI!! Jadi Student Ambassador ini bikin aku termotivasi untuk bisa jadi part of anak pacil dan benefitnya ga cuma dapet sertifikat doang tapi kita jadi bisa kenal dekat sama kaka” ui dan temen” SMA yang keren” bgtt dan juga kita sering dikasi pelatihan ambassador, soo kalian wajib bgt join ambass OH FASILKOM YESS!!”',
+    foto: '/naila.jpg',
+    namaDanSekolah: 'Naila, SMAIT ASY SYUKRIYYAH',
   },
 ]

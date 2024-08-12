3ecc407d018e2c05d3c60a394faa0f7748abcd90
gracekarinn <gracekarina12@gmail.com>
2024-08-12 15:25:04 +0700

    feat: new dataaa

--- modules/AmbassadorModule/sections/AmbassadorProgramsMobile.tsx
+++ modules/AmbassadorModule/sections/AmbassadorProgramsMobile.tsx
@@ -11,16 +11,16 @@ const AmbassadorProgramsMobile = () => {
       </p>
       <div className="flex flex-col items-center justify-center">
         {AMBASSADOR_PROGRAMS.map((program, index) => (
-          <div className="flex max-md:w-[280px] font-bold md:w-[524px] flex-col lg:flex-row lg:flex-wrap bg-gradient-custom pb-3 rounded-[50px]">
+          <div className="flex max-md:w-[280px] font-bold md:w-[524px] flex-col lg:flex-row lg:flex-wrap bg-gradient-custom pb-5 rounded-[50px]">
             <Card
               key={index}
               title=""
-              className="h-[336px] px-10 flex flex-col font-riffic leading-7 font-bold mx-auto md:w-[524px] shadow-timeline text-white rounded-[50px] mb-1 lg:mb-0 lg:mr-auto"
+              className="h-full px-10 flex flex-col font-riffic leading-7 font-bold mx-auto md:w-[524px] shadow-timeline text-white rounded-[50px] mb-1 lg:mb-0 lg:mr-auto"
             >
-              <h3 className="text-center  font-riffic tracking-wide font-bold pb-3 mb-1">
+              <h3 className="text-center font-riffic tracking-wide font-bold pb-3 mb-1">
                 {program.title}
               </h3>
-              <p className="text-xs font-tex-gyre ">{program.description}</p>
+              <p className="text-xs font-tex-gyre">{program.description}</p>
             </Card>
           </div>
         ))}

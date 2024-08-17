be9a120658347597748f9ab666a6d9d949d578b1
Tristan A. <tristanagraa@gmail.com>
2024-08-17 21:56:30 +0700

    fix: scroll bar thumb color

--- components/ui/ScrollArea.tsx
+++ components/ui/ScrollArea.tsx
@@ -40,7 +40,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-[#4756e4]" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName

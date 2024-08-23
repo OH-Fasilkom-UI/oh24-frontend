1f7476d56a972b0a0399573aced3ae6781c2ee24
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-08-23 09:29:58 +0700

    fix: non hd image in ambassador help

--- modules/AmbassadorModule/sections/AmbassadorHelp.tsx
+++ modules/AmbassadorModule/sections/AmbassadorHelp.tsx
@@ -9,7 +9,7 @@ const AmbassadorHelp = () => {
           alt="city"
           width={1440}
           height={400}
-          className="w-[1000px] max-md:hidden block object-contain -translate-x-10 lg:-translate-x-24"
+          className="w-[1000px] max-md:hidden block object-contain -translate-x-10 lg:-translate-x-0"
         />
         <Image
           src="/city-amb.png"
@@ -23,7 +23,7 @@ const AmbassadorHelp = () => {
         <h1 className="text-[48px] max-md:text-[24px] max-xl:text-[30px] whitespace-nowrap font-riffic mb-3 leading-[48px] tracking-widest text-[#B91B70] font-bold text-center">
           Need more help?
         </h1>
-        <p className="lg:text-[20px] max-md:text-[14px] leading-[20px] font-tex-gyre font-bold text-center text-[#530C32]">
+        <p className="lg:text-[20px] max-md:text-[14px] leading-7 font-tex-gyre font-bold text-center text-[#530C32]">
           Temukan berbagai pertanyaan serta jawaban seputar ambassador di sini!
         </p>
       </div>

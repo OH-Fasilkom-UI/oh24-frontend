2b05643a5e4d07805842e301c8db658a74cb8590
kevincornellius <kevincornelius1905@gmail.com>
2024-09-08 22:27:53 +0700

    fix: responsiveness

--- modules/LandingPageModule/components/LandingPageHero.tsx
+++ modules/LandingPageModule/components/LandingPageHero.tsx
@@ -6,8 +6,8 @@ import { SquarePen } from 'lucide-react'
 
 const LandingPageHero = () => {
   return (
-    <div className="xl:mt-[194px] max-sm:mt-[100px] sm:mt-[120px] flex flex-col items-center">
-      <h2 className="font-bold text-center text-Text/TextLightBG leading-[50px] text-4xl max-sm:text-3xl max-sm:leading-[29px] text-[48px] tracking-wider">
+    <div className="mt-[100px] sm:mt-[180px] flex flex-col items-center">
+      <h2 className="font-bold text-center text-Text/TextLightBG leading-[50px] text-[26px] sm:text-t2 max-sm:leading-[29px] tracking-wider">
         Open House
         <br /> &nbsp;Fasilkom UI 2024
       </h2>
@@ -15,17 +15,17 @@ const LandingPageHero = () => {
         <Countdown
           targetDate={new Date('2024-09-19T23:59:00')}
           displayDate
-          classNameType="text-md sm:text-xl text-white"
-          classNameBlock="w-20 h-24 sm:w-28 sm:h-32 mt-3 mb-3 sm:mb-8"
+          classNameType="sm:text-xl text-white"
+          classNameBlock=" w-[60px] h-[70px] sm:w-20 sm:h-24 sm:w-28 sm:h-32 mt-3 mb-3 sm:mb-8"
         />
       </div>
-      <div className="flex gap-4 sm:gap-12 my-12">
-        <Button className=" w-[150px] sm:w-[200px] text-[12px]">
+      <div className="flex gap-4 sm:gap-12 my-6 sm:my-12">
+        <Button className=" w-[130px] sm:w-[200px] text-[10px] sm:text-[12px]">
           About OH 2024
         </Button>
         <Button
           variant="tertiary"
-          className="w-[140px] sm:w-[180px] max-md:text-[12px]"
+          className="w-[120px] h-[40px] sm:w-[180px] sm:h-[50px] text-[10px] sm:text-[12px]"
         >
           Register
           <SquarePen />

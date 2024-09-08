5fb11ad404a94fbe484b25f1150e6c21acbdfd75
Fazil <faziltirtana@tutanota.com>
2024-09-08 20:48:50 +0700

    feat: create modules and routing

---
+++ app/register/page.tsx
@@ -0,0 +1,13 @@
+import Footer from '@/components/elements/Footer'
+import { MenteeRegistrationModule } from '@/modules/MenteeRegistrationModule'
+
+const Page = () => {
+  return (
+    <main>
+      <MenteeRegistrationModule />
+      <Footer />
+    </main>
+  )
+}
+
+export default Page
---
+++ modules/MenteeRegistrationModule/index.tsx
@@ -0,0 +1,5 @@
+export const MenteeRegistrationModule = () => {
+    return (
+        <h1>Harap mengisi kuesioner dengan serius karena akan memengaruhi step selanjutnya.</h1>
+    )
+}

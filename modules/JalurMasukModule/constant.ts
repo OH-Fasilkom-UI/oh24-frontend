21fd14898ff7963a3d2922bf77c5ea67cb5ade41
gracekarinn <gracekarina12@gmail.com>
2024-09-19 18:11:33 +0700

    feat: add constants

--- modules/JalurMasukModule/constant.ts
+++ modules/JalurMasukModule/constant.ts
@@ -20,3 +20,39 @@ export const SISTEM_INFORMASI = [
       'Buat kamu yang aktif di sekolah dan punya prestasi akademik plus pengalaman organisasi, PPKB bisa jadi jalur masuk andalan. Jalur ini khusus buat siswa yang direkomendasikan sekolah (dan hanya untuk sekolah-sekolah tertentu…), jadi pastikan kamu termasuk di dalamnya!',
   },
 ]
+
+export const ISI_ILMU_KOMPUTER = [
+  {
+    title: 'SNBP (Seleksi Nasional Berdasarkan Prestasi) ',
+    description:
+      'Buat kamu yang jago di kelas dan punya nilai rapor keren, ini jalur masuk tanpa tes yang bisa kamu andalkan! Cukup tunjukin prestasi akademikmu selama sekolah, dan kamu berpeluang besar jadi bagian dari Fasilkom UI.',
+  },
+  {
+    title: 'SNBT (Seleksi Nasional Berdasarkan Tes)',
+    description:
+      'Ini jalur masuk yang paling umum, di mana kamu bakal diuji kemampuan akademik lewat tes nasional. Siapkan otakmu untuk matematika, bahasa, dan logika. Kalo suka tantangan, ini pilihan pas buatmu!',
+  },
+  {
+    title: 'SIMAK UI',
+    description:
+      'Mau masuk UI lewat jalur mandiri? SIMAK UI jawabannya! Ujian ini khusus buat kamu yang pengen langsung ngejar kursi di Fasilkom UI, dengan materi ujian yang sedikit beda tapi tetep menantang. Jalur SIMAK juga menyediakan kursi untuk KKI lhoo…',
+  },
+  {
+    title: 'PPKB (Prestasi dan Pemerataan Kesempatan Belajar)',
+    description:
+      'Buat kamu yang aktif di sekolah dan punya prestasi akademik plus pengalaman organisasi, PPKB bisa jadi jalur masuk andalan. Jalur ini khusus buat siswa yang direkomendasikan sekolah (dan hanya untuk sekolah-sekolah tertentu…), jadi pastikan kamu termasuk di dalamnya!',
+  },
+]
+
+export const KKI_ILMU_KOMPUTER = [
+  {
+    title: 'Talent Scouting',
+    description:
+      'Pengen merasakan sensasi kuliah dengan kurikulum internasional? Di jalur ini, kamu bisa belajar dengan standar global dan berkolaborasi dengan universitas luar negeri. Siapkan tes bahasa Inggris-mu seperti TOEFL atau IELTS dan bersiaplah untuk pengalaman belajar yang berbeda!',
+  },
+  {
+    title: 'SIMAK UI',
+    description:
+      'Mau masuk UI lewat jalur mandiri? SIMAK UI jawabannya! Ujian ini khusus buat kamu yang pengen langsung ngejar kursi di Fasilkom UI, dengan materi ujian yang sedikit beda tapi tetep menantang. Jalur SIMAK juga menyediakan kursi untuk KKI lhoo…',
+  },
+]

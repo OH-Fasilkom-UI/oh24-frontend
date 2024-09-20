e2997e1674e82d4592638a9d3571f8c3ad8ad485
Hakim Nizami <hakimnizami15@gmail.com>
2024-09-21 02:02:12 +0700

    feat: created explore fasilkom facility section

---
+++ modules/ExploreModule/index.tsx
@@ -0,0 +1,13 @@
+import ExploreFacility from './components/ExploreFacility'
+
+const ExploreModule = () => {
+  return (
+    <main className="flex flex-col z-20 w-full relative pt-20 overflow-x-hidden">
+      <div className="relative flex-grow">
+        <ExploreFacility />
+      </div>
+    </main>
+  )
+}
+
+export default ExploreModule

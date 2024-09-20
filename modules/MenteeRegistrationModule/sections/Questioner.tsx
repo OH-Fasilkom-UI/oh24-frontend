cdb200bc2b7fdb07fdec81efc693a320ecd9bab3
Tristan A. <tristanagraa@gmail.com>
2024-09-20 18:39:19 +0700

    feat: finish slicing(static)

--- modules/MenteeRegistrationModule/sections/Questioner.tsx
+++ modules/MenteeRegistrationModule/sections/Questioner.tsx
@@ -1,7 +1,8 @@
 'use client'
 import React from 'react'
-import bg from '@/public/KotaBelakang.png'
-import bg2 from '@/public/KotaDikit.png'
+import bg2 from '@/public/KotaBelakang.png'
+import bg from '@/public/KotaDikit.png'
+import Image from 'next/image'
 import {
   submitQuestionnaireSchema,
   SubmitQuestionnaireData,
@@ -23,8 +24,11 @@ import {
   InterestRadio,
 } from '../elements/RadioItem'
 import { Accommodation, Companion } from '@/lib/api/registration-mentee'
+import { cn } from '@/lib/utils'
 
 export const Questioner = () => {
+  const isJabodetabek = false
+
   const form = useForm<SubmitQuestionnaireData>({
     resolver: zodResolver(submitQuestionnaireSchema),
   })
@@ -32,11 +36,14 @@ export const Questioner = () => {
   const onSubmit = async (data: SubmitQuestionnaireData) => {}
   return (
     <section
-      className="min-h-[100vh] w-full flex justify-center items-center"
+      className={cn(
+        'min-h-[100vh] w-full px-[10vw] flex justify-center items-center',
+        !isJabodetabek && 'py-[20vh]'
+      )}
       style={{
-        backgroundImage: `url(${bg2.src})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
+        backgroundImage: `url(${isJabodetabek ? bg2.src : bg.src})`,
+        backgroundSize: isJabodetabek ? 'cover' : 'contain',
+        backgroundPosition: 'bottom',
         backgroundRepeat: 'no-repeat',
       }}
     >
@@ -45,13 +52,13 @@ export const Questioner = () => {
           onSubmit={form.handleSubmit(onSubmit)}
           className="flex flex-col gap-6"
         >
-          <div className="rounded-[32px] bg-Text/TextLightBG/90 shadow-timeline px-[70px] py-10 max-w-[660px]">
+          <div className="rounded-[32px] bg-Text/TextLightBG/90 shadow-timeline px-[30px] md:px-[70px] py-8 md:py-10 max-w-[660px] space-y-[34px]">
             <FormField
               control={form.control}
               name={'answers.0'}
               render={({ field }) => (
                 <FormItem>
-                  <h1 className="text-Text/TextDarkBG font-bold text-t7 text-center font-tex-gyre tracking-wider mb-[18px]">
+                  <h1 className="text-Text/TextDarkBG font-bold text-t7 max-sm:text-[10px] text-center font-tex-gyre tracking-wider mb-[18px] max-sm:mb-[14px]">
                     Seberapa tertarik Kamu terhadap bidang Ilmu Komputer/Sistem
                     Informasi/Teknologi
                   </h1>
@@ -59,7 +66,7 @@ export const Questioner = () => {
                     <RadioGroup
                       size="small"
                       color="light"
-                      className="grid grid-rows-2 grid-flow-col gap-[26px] justify-center items-center tracking-wide"
+                      className="grid sm:grid-rows-2 grid-rows-3 grid-flow-col md:gap-[26px] gap-2 sm:justify-center items-center tracking-wide max-sm:text-[8px]"
                       onValueChange={(value: string) =>
                         field.onChange(value as unknown as Number)
                       }
@@ -73,20 +80,20 @@ export const Questioner = () => {
                 </FormItem>
               )}
             />
-            <hr className="my-9" />
+            <hr className="my-9 max-sm:hidden" />
             <FormField
               control={form.control}
               name={'answers.1'}
               render={({ field }) => (
                 <FormItem>
-                  <h1 className="text-Text/TextDarkBG font-bold text-t7 text-center font-tex-gyre tracking-wider mb-[18px]">
+                  <h1 className="text-Text/TextDarkBG font-bold text-t7 max-sm:text-[10px] text-center font-tex-gyre tracking-wider mb-[18px] max-sm:mb-[14px]">
                     Seberapa besar keinginan Kamu untuk masuk Fasilkom UI
                   </h1>
                   <FormControl>
                     <RadioGroup
                       size="small"
                       color="light"
-                      className="grid grid-rows-2 grid-flow-col gap-[26px] justify-center items-center tracking-wide"
+                      className="grid sm:grid-rows-2 grid-rows-3 grid-flow-col md:gap-[26px] gap-2 sm:justify-center items-center tracking-wide max-sm:text-[8px]"
                       onValueChange={(value: string) =>
                         field.onChange(value as unknown as Number)
                       }
@@ -100,13 +107,13 @@ export const Questioner = () => {
                 </FormItem>
               )}
             />
-            <hr className="my-9" />
+            <hr className="my-9 max-sm:hidden" />
             <FormField
               control={form.control}
               name={'answers.2'}
               render={({ field }) => (
                 <FormItem>
-                  <h1 className="text-Text/TextDarkBG font-bold text-t7 text-center font-tex-gyre tracking-wider mb-[18px]">
+                  <h1 className="text-Text/TextDarkBG font-bold text-t7 max-sm:text-[10px] text-center font-tex-gyre tracking-wider mb-[18px] max-sm:mb-[14px]">
                     Seberapa besar keinginan Kamu untuk mengikuti rangkaian
                     acara Open House Fasilkom UI
                   </h1>
@@ -114,7 +121,7 @@ export const Questioner = () => {
                     <RadioGroup
                       size="small"
                       color="light"
-                      className="grid grid-rows-2 grid-flow-col gap-[26px] justify-center items-center tracking-wide"
+                      className="grid sm:grid-rows-2 grid-rows-3 grid-flow-col md:gap-[26px] gap-2 sm:justify-center items-center tracking-wide max-sm:text-[8px]"
                       onValueChange={(value: string) =>
                         field.onChange(value as unknown as Number)
                       }
@@ -128,66 +135,92 @@ export const Questioner = () => {
                 </FormItem>
               )}
             />
-            <hr className="my-9" />
-            <FormField
-              control={form.control}
-              name={'accommodation'}
-              render={({ field }) => (
-                <FormItem>
-                  <h1 className="text-Text/TextDarkBG font-bold text-t7 text-center font-tex-gyre tracking-wider mb-[18px]">
-                    Apabila ingin mengikuti Main Event yang berada di Fasilkom
-                    UI, Depok pada tanggal 16 November 2024 atau 17 November
-                    2024, akomodasi yang digunakan?
-                  </h1>
-                  <FormControl>
-                    <RadioGroup
-                      size="small"
-                      color="light"
-                      className="flex flex-row gap-[26px] justify-center items-center tracking-wide"
-                      onValueChange={(value: string) =>
-                        field.onChange(value as unknown as Accommodation)
-                      }
-                      {...field}
-                      value={field.value as unknown as string}
-                    >
-                      <AccomodationRadio />
-                    </RadioGroup>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <hr className="my-9" />
-            <FormField
-              control={form.control}
-              name={'companion'}
-              render={({ field }) => (
-                <FormItem>
-                  <h1 className="text-Text/TextDarkBG font-bold text-t7 text-center font-tex-gyre tracking-wider mb-[18px]">
-                    Apabila ingin mengikuti Main Event yang berada di Fasilkom
-                    UI, Depok pada tanggal 16 November 2024 atau 17 November
-                    2024, mengunjungi dengan siapa?
-                  </h1>
-                  <FormControl>
-                    <RadioGroup
-                      size="small"
-                      color="light"
-                      className="flex flex-row gap-[26px] justify-center items-center tracking-wide"
-                      onValueChange={(value: string) =>
-                        field.onChange(value as unknown as Companion)
-                      }
-                      {...field}
-                      value={field.value as unknown as string}
-                    >
-                      <CompanionRadio />
-                    </RadioGroup>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            {!isJabodetabek && (
+              <>
+                <div className="absolute max-lg:w-[410px] max-md:w-[300px] max-[400px]:w-[300px] max-[400px]:h-[100px] w-[500px] h-[400px] right-0 top-[40vh] max-xl:hidden z-10 animate-float-vertical">
+                  <Image
+                    alt="contoh"
+                    src="/PesawatBiru.png"
+                    fill
+                    sizes="none"
+                    className="object-contain"
+                  />
+                </div>
+                <div className="absolute max-lg:w-[410px] max-md:w-[300px] max-[400px]:w-[300px] max-[400px]:h-[100px] w-[500px] h-[400px] left-[10vw] top-[40vh] max-xl:hidden -z-10 animate-float-vertical">
+                  <Image
+                    alt="contoh"
+                    src="/pesawat-kanan-2.png"
+                    fill
+                    sizes="none"
+                    className="object-contain"
+                  />
+                </div>
+                <hr className="my-9 max-sm:hidden" />
+                <FormField
+                  control={form.control}
+                  name={'accommodation'}
+                  render={({ field }) => (
+                    <FormItem>
+                      <h1 className="text-Text/TextDarkBG font-bold text-t7 max-sm:text-[10px] text-center font-tex-gyre tracking-wider mb-[18px] max-sm:mb-[14px]">
+                        Apabila ingin mengikuti Main Event yang berada di
+                        Fasilkom UI, Depok pada tanggal 16 November 2024 atau 17
+                        November 2024, akomodasi yang digunakan?
+                      </h1>
+                      <FormControl>
+                        <RadioGroup
+                          size="small"
+                          color="light"
+                          className="flex flex-col sm:flex-row md:gap-[26px] gap-2 justify-center md:items-center tracking-wide max-sm:text-[8px]"
+                          onValueChange={(value: string) =>
+                            field.onChange(value as unknown as Accommodation)
+                          }
+                          {...field}
+                          value={field.value as unknown as string}
+                        >
+                          <AccomodationRadio />
+                        </RadioGroup>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <hr className="my-9 max-sm:hidden" />
+                <FormField
+                  control={form.control}
+                  name={'companion'}
+                  render={({ field }) => (
+                    <FormItem>
+                      <h1 className="text-Text/TextDarkBG font-bold text-t7 max-sm:text-[10px] text-center font-tex-gyre tracking-wider mb-[18px] max-sm:mb-[14px]">
+                        Apabila ingin mengikuti Main Event yang berada di
+                        Fasilkom UI, Depok pada tanggal 16 November 2024 atau 17
+                        November 2024, mengunjungi dengan siapa?
+                      </h1>
+                      <FormControl>
+                        <RadioGroup
+                          size="small"
+                          color="light"
+                          className="flex flex-col sm:flex-row md:gap-[26px] gap-2 justify-center md:items-center tracking-wide max-sm:text-[8px]"
+                          onValueChange={(value: string) =>
+                            field.onChange(value as unknown as Companion)
+                          }
+                          {...field}
+                          value={field.value as unknown as string}
+                        >
+                          <CompanionRadio />
+                        </RadioGroup>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </>
+            )}
           </div>
-          <Button type="submit" className="w-full" variant={'ghost'}>
+          <Button
+            type="submit"
+            className="w-full max-sm:w-[90px] mx-auto"
+            variant={'ghost'}
+          >
             Submit
           </Button>
         </form>
--- modules/MenteeRegistrationModule/sections/WarnModal.tsx
+++ modules/MenteeRegistrationModule/sections/WarnModal.tsx
@@ -14,29 +14,21 @@ import Image from 'next/image'
 export const WarnModal = ({ onClick }: { onClick: () => void }) => {
   return (
     <div className="relative">
-      <Dialog open>
-        <DialogContent
-          showCloseButton
-          notBlur
-          className="max-sm:w-[190px] sm:rounded-[24px] rounded-[18px]"
-        >
-          <DialogHeader>
-            <DialogTitle className="max-sm:mx-auto">
-              <AlertTriangle size={48} className="text-Text/TextLightBG" />
-            </DialogTitle>
-            <DialogDescription className="text-Text/TextLightBG sm:font-bold font-normal max-md:text-center">
-              Harap mengisi kuesioner dengan serius karena akan memengaruhi step
-              selanjutnya.
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <Button className="max-sm:mx-auto" onClick={onClick}>
-              Next
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-      <div className="absolute w-[304px] h-[304px] max-[400px]:w-[240px] bottom-0 -right-[152px] max-[400px]:-right-[130px] sm:hidden animate-spin">
+      <div className="max-md:w-[190px] md:rounded-[24px] rounded-[18px] flex flex-col px-8 py-9 bg-[#F0F8FF]">
+        <div className="max-md:mx-auto">
+          <AlertTriangle size={48} className="text-Text/TextLightBG" />
+        </div>
+        <p className="text-Text/TextLightBG sm:font-bold font-normal max-md:text-center my-2">
+          Harap mengisi kuesioner dengan serius karena akan memengaruhi step
+          selanjutnya.
+        </p>
+        <div className='flex justify-end max-sm:mx-auto'>
+          <Button className="mt-4" onClick={onClick}>
+            Next
+          </Button>
+        </div>
+      </div>
+      <div className="absolute w-[304px] h-[304px] max-[400px]:w-[240px] -top-[140px] -right-[55px] max-[400px]:-right-[20px] md:hidden animate-spin -z-10">
         <Image
           alt="contoh"
           src="/Circular.png"
@@ -45,7 +37,7 @@ export const WarnModal = ({ onClick }: { onClick: () => void }) => {
           className="object-contain"
         />
       </div>
-      <div className="absolute max-lg:w-[410px] max-sm:w-[300px] max-[400px]:w-[250px] max-[400px]:h-[350px] w-[500px] h-[400px] -left-[87px] max-sm:-left-[120px] top-5 z-10 animate-float-vertical">
+      <div className="absolute max-lg:w-[410px] max-md:w-[300px] max-[400px]:w-[300px] max-[400px]:h-[100px] w-[500px] h-[400px] lg:-right-[120px] md:-right-[40px] max-md:-right-[75px] top-[220px] md:top-[120px] -z-10 animate-float-vertical">
         <Image
           alt="contoh"
           src="/Whooshhh.png"

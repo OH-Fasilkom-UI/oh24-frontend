a642217aeb4597b9b9e18be127b919008a67b197
Tristan A. <tristanagraa@gmail.com>
2024-09-28 01:35:13 +0700

    feat: page logic

--- modules/RegisterPickEventModule/index.tsx
+++ modules/RegisterPickEventModule/index.tsx
@@ -2,11 +2,11 @@
 import { PickEventSection } from '@/modules/RegisterPickEventModule/sections/PickEvent'
 import RegisterAccordion from './sections/RegisterAccordion'
 
-export const PickEventModule = () => {
-    return (
-        <section className="max-w-[1920px] h-fit py-[20rem] pt-[100px] overflow-x-hidden">
-            <PickEventSection />
-            <RegisterAccordion />
-        </section>
-    )
+export const PickEventModule = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <section className="max-w-[1920px] h-fit py-[20rem] pt-[100px] overflow-x-hidden">
+      <PickEventSection onClick={() => onClick()} />
+      <RegisterAccordion />
+    </section>
+  )
 }

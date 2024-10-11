a8ba0baa8938e6e065d5bc9fe89234537d2e840a
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-10-11 21:53:25 +0700

    feat: add more copywriting to regis faq

--- modules/RegisterPickEventModule/sections/RegisterAccordion.tsx
+++ modules/RegisterPickEventModule/sections/RegisterAccordion.tsx
@@ -9,18 +9,47 @@ import {
 
 export const EventFAQAccordion = [
   {
-    question:
-      'Kak, aku ingin merasakan pengalaman offline tour di Fasilkom! Pilih yang mana ya?',
+    question: 'Apakah saya harus mendaftar dulu?',
     answer:
-      'Kamu bisa memilih opsi Full Experience! Dengan memilih Full Experience, kamu dapat mengikuti pengalaman penuh yaitu CS Connect secara online via Zoom (9 November 2024) dan Main Event secara offline di gedung lama Fasilkom!',
+      'Ya, peserta diharuskan mendaftar terlebih dahulu melalui form pendaftaran yang sudah disediakan di Website Open House Fasilkom UI. Pastikan kamu mengecek website atau media sosial resmi Open House Fasilkom UI untuk detail pendaftaran.',
   },
   {
-    question:
-      'Kak, aku belum bisa hadir secara offline nih.. Tapi kepengen ikutan :(',
+    question: 'Apa saja yang bisa saya harapkan di Open House ini?',
     answer:
-      'Gapapa! Kamu bisa memilih opsi Online Experience. Kamu tetap bisa mengikuti CS Connect, yaitu talkshow dan mentoring secara online dengan mentor-mentor Open House Fasilkom 2024.',
+      'Kamu dapat mengharapkan banyak hal, seperti:\n1. Fun Coding: Belajar coding dengan cara interaktif dan menyenangkan.\n2. Mentoring Sessions: Mendapatkan informasi dari mahasiswa Fasilkom UI tentang kehidupan perkuliahan.\n3. CS Connect: Talkshow online untuk pengenalan Fasilkom UI.\n4. Tour de Campus: Tur kampus Fasilkom dengan mentor.\n5. Interactive Class: Kelas interaktif dengan dosen Fasilkom UI.\n6. Exhibition: Booth jurusan dan organisasi.\n7. Wall of Fame: Profil singkat individu inspiratif dari Fasilkom UI.\n8. Parent\'s Talkshow: Sesi untuk orang tua terkait penerimaan mahasiswa baru dan biaya kuliah.\n9. Wall of Dream: Pameran interaktif di mana peserta dapat menuliskan impian mereka.',
   },
-]
+  {
+    question: 'Apakah ada sesi konsultasi tentang jalur masuk Fasilkom UI?',
+    answer:
+      'Ya, akan ada mentoring session di mana kamu bisa membahas berbagai jalur masuk seperti SNBP, SNBT, SIMAK UI, dan lainnya. Kamu juga bisa berkonsultasi langsung dengan mentor melalui kegiatan ini.',
+  },
+  {
+    question: 'Bagaimana kalau saya tidak bisa datang langsung ke UI?',
+    answer:
+      'Jika tidak bisa hadir secara langsung, kamu bisa mengikuti acara CS Connect yang diadakan secara online pada tanggal 9 November 2024. Acara ini mirip dengan main event tetapi dilakukan secara virtual.',
+  },
+  {
+    question: 'Apa saja yang perlu saya siapkan sebelum datang ke acara ini?',
+    answer:
+      'Persiapkan pertanyaan tentang program studi, jalur masuk, atau aspek lain yang ingin kamu ketahui. Bawa alat tulis atau gadget untuk mencatat informasi penting dan cek jadwal acara agar lebih siap.',
+  },
+  {
+    question: 'Di mana saya bisa mendapatkan info lebih lanjut?',
+    answer:
+      'Informasi lebih lanjut bisa kamu dapatkan melalui website resmi dan media sosial Open House Fasilkom UI: Instagram (@ohfasilkom), Twitter (@OHFasilkom), TikTok (@openhousefasilkomui), YouTube (Fasilkom UI).',
+  },
+  {
+    question: 'Di Open House Fasilkom UI ngapain aja sih?',
+    answer:
+      'Banyak kegiatan seperti Fun Coding, Interactive Class, Tour de Campus, Exhibition, Parent\'s Talkshow, Mentoring Sessions, serta Wall of Fame yang menampilkan profil alumni Fasilkom UI.',
+  },
+  {
+    question: 'Siapa saja yang boleh datang ke Open House ini?',
+    answer:
+      'Open House ini terbuka untuk siswa SMA atau sederajat, siswa gap year/semi gap year, serta orang tua murid yang ingin mendapatkan informasi lebih lanjut. Akan ada seminar Parent\'s Talkshow pada acara puncak di tanggal 23 November 2024.',
+  },
+];
+
 
 const RegisterAccordion = () => {
   return (

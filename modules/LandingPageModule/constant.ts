c783338e91812931af6cb7cb6cd0178a6df9ba4d
secona <77039267+secona@users.noreply.github.com>
2024-10-13 21:12:07 +0700

    fix: testimoni

--- modules/LandingPageModule/constant.ts
+++ modules/LandingPageModule/constant.ts
@@ -30,7 +30,7 @@ export const WHAT_IS_OH_DATA: WhatIsOpenHouseItemProps[] = [
 export const TESTIMONY_DATA = [
   {
     name: 'Oscar Glad Winfi',
-    foto: '/testimony/OscarSimanullang.jpg',
+    foto: '/Testimony/OscarSimanullang.jpg',
     testimonial:
       '“Menurutku pengalaman OH 2023 memberikan motivasi tersendiri bagiku untuk semakin semangat mengejar Fasilkom UI di tahun 2024. Selain itu, OH juga menjadi pengalaman menarik bagiku karena banyak info tentang Pacil yang baru aku ketahui dengan mengikuti OH 2023”',
     namaDanSekolah: 'Full Experience',
@@ -39,42 +39,42 @@ export const TESTIMONY_DATA = [
     name: 'Muhammad Azka Awliya',
     testimonial:
       '“Seru, menyenangkan, dan tentunya kakak2 mentornya ramah2. Selain itu, informasi yang diberikan juga informatif dan insightful. Acaranya seru banget apalagi pas tour de campus dan fun coding. 10/10 ”',
-    foto: '/testimony/MuhammadAzkaAwliya.jpeg',
+    foto: '/Testimony/MuhammadAzkaAwliya.jpeg',
     namaDanSekolah: 'Full Experience',
   },
   {
     name: 'Naomyscha Attalie Maza',
     testimonial:
       '“Sooo funn! Bisa ketemu temen-temen baru seperjuangan yang juga mau masuk Fasilkom UI bahkan sampai sekarang masih temenan dan bareng2 di pacil!! Superr insightful dan useful untuk persiapan masuk Fasilkom especially kegiatan mentoring & CSConnect nyaa!! I learned so much more about the curriculum and all the activities at Fasilkom.”',
-    foto: '/testimony/NaomyschaAttalieMaza.jpeg',
+    foto: '/Testimony/NaomyschaAttalieMaza.jpeg',
     namaDanSekolah: 'Full Experience',
   },
   {
     name: 'Jessica Tandra',
     testimonial:
       '“Menurutku, ikut Open House Fasilkom UI itu bener-bener bermanfaat dan superr funn! Tahun lalu aku adalah anak SMA yang masih bimbang mau lanjut kuliah kemana... yaudah deh aku coba Ikut OH Fasilkom. Ternyata OH itu bikin aku makin mengenal lebih dalam tentang semua civitas akademik maupun non-akademik di Fasilkom, baik itu kehidupan perkuliahan, tour gedung Fasilkom yang superr keren, bahkan kita dikasih kesempatan buat ngerasain jadi anak Fasilkom dalam sehari WOWW... Nah setelah ikut OH jadi ngga bingung lagi dehh, langsung yakin pokonya mau masuk FASILKOM UI!! TITIK.”',
-    foto: '/testimony/JessicaTandra.jpg',
+    foto: '/Testimony/JessicaTandra.jpg',
     namaDanSekolah: 'Full Experience',
   },
   {
     name: 'Maira Azma Shaliha',
     testimonial:
       '“Bersyukur bangett dapet info OH dan bisa langsung daftar. Jujur dari awal ikut tuh interaktif banget kakak-kakaknyaa, jadi aku juga gak sungkan buat ikutan aktif. Pas CS Connect tuh informatif banget dan penjelasannya juga asik bikin enjoyy. Naah, pas Tour de Campus tuh makin seru lagi soalnya bener-bener keliling gedung baru sama gedung lama, sambil ada materi juga. Jadi makin tau prestasi sama organisasi di Pacil tuh literally banyak bgt. OH 23 jadi peserta OH 24 jadi panitia CHUAKZZ #Well😎”',
-    foto: '/testimony/MairaAzma.jpg',
+    foto: '/Testimony/MairaAzma.jpg',
     namaDanSekolah: 'Full Experience',
   },
   {
     name: 'Vazha Khayri',
     testimonial:
       '“Asik banget!! Waktu itu mau liat gimana sih fasilkom ui dan beneran diajak tour campus, dikenalin kehidupan kampusnya kayak gimana, dan kaka kaka mentornya pun ngejelasinnya jelas. Jadi, sekarang saat aku udah kuliah di fasilkom udah lumayan paham dan mengerti kehidupan kampusnya dan hafal gedungnya hehe...”',
-    foto: '/testimony/VazhaK.jpg',
+    foto: '/Testimony/VazhaK.jpg',
     namaDanSekolah: 'Full Experience',
   },
   {
     name: 'Rafalah Izak',
     testimonial:
       '“Sangat bermanfaat, banyak banget ilmu mahal yang dibagi secara cuma cuma sama mentor mentornya. Pokoknya gak mungkin rugi dah ikut OH Fasilkom karena gw aja jadi yakin masuk Fasilkom gara gara acara OH ini. ”',
-    foto: '/testimony/RafalahIzak.jpg',
+    foto: '/Testimony/RafalahIzak.jpg',
     namaDanSekolah: 'Full Experience',
   },
 ]

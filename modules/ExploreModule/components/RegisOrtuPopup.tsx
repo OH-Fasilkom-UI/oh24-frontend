a4882a9fa0c70fc4d81fe170d38cf3e2d9b9e62a
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-10-14 08:03:12 +0700

    feat: regist ortu pop up

---
+++ modules/ExploreModule/components/RegisOrtuPopup.tsx
@@ -0,0 +1,32 @@
+'use client'
+import Image from "next/image"
+import Link from "next/link";
+import { useEffect, useState } from "react"
+
+export const RegistOrtuPopup = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        setIsVisible(true);
+    }, []);
+
+    return (
+        <Link
+            href={"https://docs.google.com/forms/d/e/1FAIpQLScnQxFQbK4V1i15D653HlzzwF2Iupo1zUcHCwVXYYnZqUUL0g/viewform"}
+            passHref={true}
+            target="_blank"
+            className="z-50"
+        >
+            <div className={`fixed z-50 left-5 top-32 cursor-pointer flex flex-col items-center transition-all duration-1000 transform ${isVisible ? 'translate-x-0 opacity-100 animate-swing' : '-translate-x-full opacity-0'}`}>
+                <Image
+                    src="/Profile/cowo1.png"
+                    width={300}
+                    height={300}
+                    alt="Register Ortu Popup"
+                    className="w-32 h-32"
+                />
+                <h1 className="w-[200px] text-center bg-white rounded-full py-2">Registrasi Parent's Talkshow</h1>
+            </div>
+        </Link>
+    )
+}

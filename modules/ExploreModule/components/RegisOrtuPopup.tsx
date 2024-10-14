e52e069e14d8426ba6e97823052e048a1eb1e51c
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-10-14 11:57:48 +0700

    cls

--- modules/ExploreModule/components/RegisOrtuPopup.tsx
+++ modules/ExploreModule/components/RegisOrtuPopup.tsx
@@ -18,14 +18,18 @@ export const RegistOrtuPopup = () => {
             className="z-50"
         >
             <div className={`fixed z-50 left-5 top-32 cursor-pointer flex flex-col items-center transition-all duration-1000 transform ${isVisible ? 'translate-x-0 opacity-100 animate-swing' : '-translate-x-full opacity-0'}`}>
-                <Image
-                    src="/Profile/cowo1.png"
-                    width={300}
-                    height={300}
-                    alt="Register Ortu Popup"
-                    className="w-32 h-32"
-                />
-                <h1 className="w-[200px] text-center bg-white rounded-full py-2">Registrasi Parent's Talkshow</h1>
+                <div
+                    className="border-2 border-white rounded-full bg-white p-1"
+                >
+                    <Image
+                        src="/Profile/cowo1.png"
+                        width={300}
+                        height={300}
+                        alt="Register Ortu Popup"
+                        className="w-32 h-32 "
+                    />
+                </div>
+                <h1 className="w-[200px] text-center bg-white rounded-full py-2 text-Text/TextLightBG">Registrasi Parent's Talkshow</h1>
             </div>
         </Link>
     )

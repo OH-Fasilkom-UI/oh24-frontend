cc283caf75c6d1d239b806ab34abe4d89e302cf5
secona <77039267+secona@users.noreply.github.com>
2024-10-23 17:12:27 +0700

    fix: redirect

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,12 +1,11 @@
-import { redirect } from 'next/navigation'
+import { MenteeRegistrationModule } from "@/modules/MenteeRegistrationModule"
 
 const Page = () => {
-  redirect('/')
-  // return (
-  //   <main>
-  //     <MenteeRegistrationModule />
-  //   </main>
-  // )
+  return (
+    <main>
+      <MenteeRegistrationModule />
+    </main>
+  )
 }
 
 export default Page

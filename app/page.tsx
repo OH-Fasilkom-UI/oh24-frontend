c7e027f1e9fa845c4ab684a0c8c5879a35700498
VazhaK14 <khayrivazhasocial@gmail.com>
2024-10-25 19:30:15 +0700

    feat/wof-hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/elements/Footer'
 import LandingPageModule from '@/modules/LandingPageModule'
+import HeroWallofFame from '@/modules/WallofFameModule/sections/WofHero'
 
 export default function Home() {
   return (
---
+++ app/wall-of-fame/page.tsx
@@ -0,0 +1,13 @@
+import HeroWallofFame from "@/modules/WallofFameModule/sections/WofHero";
+
+const Page = () => {
+    return (
+        <div className="relatiive min-h-screen">
+            <div className="z-10 relative">
+                <HeroWallofFame />
+            </div>
+        </div>
+    )
+}
+
+export default Page

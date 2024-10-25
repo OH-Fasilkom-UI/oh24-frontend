ab32ec39d50d4bd33aee60d17d8a1f9debc12009
Tristan A. <tristanagraa@gmail.com>
2024-10-25 12:09:11 +0700

    feat: init module and page

---
+++ app/merchandise/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { MerchandiseLandingModule } from '@/modules/MerchandiseLandingModule'
+
+const Merchandise = () => {
+  return <MerchandiseLandingModule />
+}
+
+export default Merchandise
---
+++ modules/MerchandiseLandingModule/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MerchandiseLandingModule = () => {
+  return (
+    <main>MerchandiseLandingModule</main>
+  )
+}

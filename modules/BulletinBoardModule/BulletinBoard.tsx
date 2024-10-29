14a89d802b8e4b8b86d9b12ea5d66c6d876ab1b7
Fazil <faziltirtana@tutanota.com>
2024-10-29 16:24:08 +0700

    feat: add bulletin board pop up modal

--- modules/BulletinBoardModule/BulletinBoard.tsx
+++ modules/BulletinBoardModule/BulletinBoard.tsx
@@ -5,39 +5,40 @@ import BoardMobile from '@/public/bulletin-board/board-mobile.png'
 import Sign from '@/public/bulletin-board/sign.png'
 import Image from 'next/image'
 import BulletinCard from './BulletinCard'
+import { BULLETIN_BOARD_DATA } from './constant'
 
 const BulletinBoard = () => {
-    return (
-        <div className='relative my-24 flex max-md:max-w-sm mx-auto'>
-            <Image
-                src={Sign}
-                alt="Sign"
-                className="absolute w-[64%] md:w-[42%] -top-[16px] md:-top-[36px] left-[50%] -translate-x-[50%] z-10"
+  return (
+    <div className="relative my-24 flex max-md:max-w-sm mx-auto">
+      <Image
+        src={Sign}
+        alt="Sign"
+        className="absolute w-[64%] md:w-[42%] -top-[16px] md:-top-[36px] left-[50%] -translate-x-[50%] z-10"
+      />
+      <Image
+        src={BoardDesktop}
+        alt="Board"
+        className="max-md:hidden w-full h-full"
+      />
+      <Image
+        src={BoardMobile}
+        alt="Board"
+        className="md:hidden w-full h-full max-w-sm"
+      />
+      <div className="absolute top-0 left-0 w-full h-full grid grid-cols-3 md:grid-cols-5 gap-3 md:gap-5 lg:gap-8 px-7 py-9 md:px-14 md:py-10 lg:px-16 lg:py-14 xl:px-20 xl:py-16">
+        {BULLETIN_BOARD_DATA.map((item) => {
+          return (
+            <BulletinCard
+              color={item.color}
+              title={item.title}
+              subtitle={item.subtitle}
+              description={item.description}
             />
-            <Image
-                src={BoardDesktop}
-                alt="Board"
-                className="max-md:hidden w-full h-full"
-            />
-            <Image
-                src={BoardMobile}
-                alt="Board"
-                className="md:hidden w-full h-full max-w-sm"
-            />
-            <div className='absolute top-0 left-0 w-full h-full grid grid-cols-3 md:grid-cols-5 gap-3 md:gap-5 lg:gap-8 px-7 py-9 md:px-14 md:py-10 lg:px-16 lg:py-14 xl:px-20 xl:py-16'>
-                <BulletinCard color='blue' text='lorem ipsum' onClick={() => { }} />
-                <BulletinCard color='red' text='lorem ipsum' onClick={() => { }} />
-                <BulletinCard color='white' text='lorem ipsum' onClick={() => { }} />
-                <BulletinCard color='yellow' text='lorem ipsum' onClick={() => { }} />
-                <BulletinCard color='blue' text='lorem ipsum' onClick={() => { }} />
-                <BulletinCard color='red' text='lorem ipsum' onClick={() => { }} />
-                <BulletinCard color='white' text='lorem ipsum' onClick={() => { }} />
-                <BulletinCard color='yellow' text='lorem ipsum' onClick={() => { }} />
-                <BulletinCard color='blue' text='lorem ipsum' onClick={() => { }} />
-                <BulletinCard color='red' text='lorem ipsum' onClick={() => { }} />
-            </div>
-        </div>
-    )
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default BulletinBoard
--- modules/BulletinBoardModule/BulletinCard.tsx
+++ modules/BulletinBoardModule/BulletinCard.tsx
@@ -1,42 +1,94 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/Dialog'
+
 const BulletinCard = ({
-    color,
-    text,
-    onClick,
+  color,
+  title,
+  subtitle,
+  description,
 }: {
-    color: string,
-    text: string,
-    onClick: (event: React.MouseEvent<HTMLDivElement>) => void,
+  color: string
+  title: string
+  subtitle: string
+  description: string
 }) => {
-    let classNameCard;
-    let classNamePin;
+  let classNameCard
+  let classNamePin
+  let backgroundColor
+  let textColor
 
-    switch (color) {
-        case 'blue':
-            classNameCard = 'bg-[#96D2FA] w-full h-[80%] -rotate-3 hover:rotate-0';
-            classNamePin = 'w-2 h-4 md:w-3 md:h-6 lg:w-4 lg:h-8 absolute -top-2 md:-top-3 lg:-top-4 left-[50%] -translate-x-[50%] bg-[#B91B70]';
-            break;
-        case 'red':
-            classNameCard = 'bg-[#F19CA3] w-[75%] h-full rotate-2 hover:-rotate-1';
-            classNamePin = 'w-3 h-3 md:w-5 md:h-5 absolute -top-1 md:-top-2 left-[50%] -translate-x-[50%] bg-[#B91B70] rounded-full';
-            break;
-        case 'yellow':
-            classNameCard = 'bg-[#C7B45D] w-[85%] h-[90%] -rotate-1 hover:rotate-2';
-            classNamePin = 'w-2 h-4 md:w-4 md:h-8 absolute -top-2 md:-top-4 left-[50%] -translate-x-[50%] bg-[#B91B70]';
-            break;
-        case 'white':
-            classNameCard = 'bg-[#D9DCE5] w-[90%] h-[85%] -rotate-2 hover:rotate-1';
-            classNamePin = 'w-3 h-3 md:w-5 md:h-5 absolute -top-1 md:-top-2 left-[50%] -translate-x-[50%] bg-[#B91B70] rounded-full';
-            break;
-        default:
-            classNameCard = 'bg-[#D9DCE5] w-[90%] h-[85%] -rotate-2 hover:-rotate-1';
-            classNamePin = 'w-3 h-3 md:w-5 md:h-5 absolute -top-1 md:-top-2 left-[50%] -translate-x-[50%] bg-[#B91B70] rounded-full';
-    }
+  switch (color) {
+    case 'blue':
+      classNameCard = 'bg-[#96D2FA] w-full h-[80%] -rotate-3 hover:rotate-0'
+      classNamePin =
+        'w-2 h-4 md:w-3 md:h-6 lg:w-4 lg:h-8 absolute -top-2 md:-top-3 lg:-top-4 left-[50%] -translate-x-[50%] bg-[#B91B70]'
+      backgroundColor = '#72b9e8'
+      textColor = 'text-white'
+      break
+    case 'red':
+      classNameCard = 'bg-[#F19CA3] w-[75%] h-full rotate-2 hover:-rotate-1'
+      classNamePin =
+        'w-3 h-3 md:w-5 md:h-5 absolute -top-1 md:-top-2 left-[50%] -translate-x-[50%] bg-[#B91B70] rounded-full'
+      backgroundColor = '#EB838C'
+      textColor = 'text-white'
+      break
+    case 'yellow':
+      classNameCard = 'bg-[#C7B45D] w-[85%] h-[90%] -rotate-1 hover:rotate-2'
+      classNamePin =
+        'w-2 h-4 md:w-4 md:h-8 absolute -top-2 md:-top-4 left-[50%] -translate-x-[50%] bg-[#B91B70]'
+      backgroundColor = '#C7B45D'
+      textColor = 'text-white'
+      break
+    case 'white':
+      classNameCard = 'bg-[#D9DCE5] w-[90%] h-[85%] -rotate-2 hover:rotate-1'
+      classNamePin =
+        'w-3 h-3 md:w-5 md:h-5 absolute -top-1 md:-top-2 left-[50%] -translate-x-[50%] bg-[#B91B70] rounded-full'
+      backgroundColor = '#D9DCE5'
+      textColor = 'text-BlueRegion/GovernorBay/600'
+      break
+    default:
+      classNameCard = 'bg-[#D9DCE5] w-[90%] h-[85%] -rotate-2 hover:-rotate-1'
+      classNamePin =
+        'w-3 h-3 md:w-5 md:h-5 absolute -top-1 md:-top-2 left-[50%] -translate-x-[50%] bg-[#B91B70] rounded-full'
+      backgroundColor = '#D9DCE5'
+      textColor = 'text-BlueRegion/GovernorBay/600'
+  }
 
-    return (
-        <div onClick={onClick} className={`cursor-pointer shadow hover:shadow-xl transition relative ${classNameCard}`}>
-            <div className={classNamePin}></div>
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <div
+          className={`cursor-pointer shadow hover:shadow-xl transition relative ${classNameCard}`}
+        >
+          <div className={classNamePin}></div>
         </div>
-    )
+      </DialogTrigger>
+      <DialogContent
+        className={`bg-[${backgroundColor}] border-[${backgroundColor}] max-w-sm`}
+        showCloseButton
+      >
+        <DialogHeader>
+          <DialogTitle
+            className={`text-center text-2xl md:text-3xl tracking-wider ${textColor}`}
+          >
+            {title}
+          </DialogTitle>
+          <DialogDescription className={textColor}>
+            <p className="text-start text-lg md:text-xl font-semibold tracking-wider py-1 md:py-3">
+              {subtitle}
+            </p>
+            <p className="text-start max-md:text-xs">{description}</p>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  )
 }
 
 export default BulletinCard

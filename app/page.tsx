f39919f3d50694f20d0608faa027f220aa8fd1d9
Fazil <faziltirtana@tutanota.com>
2024-10-31 21:06:02 +0700

    style: formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Footer from '@/components/elements/Footer'
 import LandingPageModule from '@/modules/LandingPageModule'
-import HeroWallofFame from '@/modules/WallofFameModule/sections/WofHero'
 
 export default function Home() {
   return (

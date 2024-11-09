9eccbf4a5783a46df35c49a5611ae07fdda0ed05
secona <77039267+secona@users.noreply.github.com>
2024-11-09 20:40:52 +0700

    feat: enable questionnaire

--- modules/MenteeRegistrationModule/index.tsx
+++ modules/MenteeRegistrationModule/index.tsx
@@ -4,6 +4,8 @@ import { cn } from '@/lib/utils'
 import { useState } from 'react'
 import AfterRegistrationModule from '../AfterRegistrationModule'
 import { PickEventModule } from '../RegisterPickEventModule'
+import { Questioner } from './sections/Questioner'
+import { WarnModal } from './sections/WarnModal'
 
 export const MenteeRegistrationModule = () => {
   const [page, setPage] = useState(0)
@@ -12,14 +14,15 @@ export const MenteeRegistrationModule = () => {
       <main
         className={cn(
           'flex items-center justify-center',
+          (page === 0 || page === 1) && 'min-h-screen'
         )}
       >
-        {/* {page === 0 && <WarnModal onClick={() => setPage(page + 1)} />} */}
-        {/* {page === 1 && <Questioner onClick={() => setPage(page + 1)} />} */}
+        {page === 0 && <WarnModal onClick={() => setPage(page + 1)} />}
+        {page === 1 && <Questioner onClick={() => setPage(page + 1)} />}
       </main>
       <main>
-        {page === 0 && <PickEventModule onClick={() => setPage(page + 1)} />}
-        {page === 1 && <AfterRegistrationModule />}
+        {page === 2 && <PickEventModule onClick={() => setPage(page + 1)} />}
+        {page === 3 && <AfterRegistrationModule />}
       </main>
     </>
   )

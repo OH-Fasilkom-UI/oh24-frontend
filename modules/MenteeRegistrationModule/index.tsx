e82342068da06b7a19dc9b71e51fce43c23bf352
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-11-09 13:41:08 +0700

    feat: remove warning modal

--- modules/MenteeRegistrationModule/index.tsx
+++ modules/MenteeRegistrationModule/index.tsx
@@ -4,8 +4,6 @@ import { cn } from '@/lib/utils'
 import { useState } from 'react'
 import AfterRegistrationModule from '../AfterRegistrationModule'
 import { PickEventModule } from '../RegisterPickEventModule'
-import { Questioner } from './sections/Questioner'
-import { WarnModal } from './sections/WarnModal'
 
 export const MenteeRegistrationModule = () => {
   const [page, setPage] = useState(0)
@@ -14,15 +12,14 @@ export const MenteeRegistrationModule = () => {
       <main
         className={cn(
           'flex items-center justify-center',
-          (page === 0 || page === 1) && 'min-h-screen'
         )}
       >
-        {page === 0 && <WarnModal onClick={() => setPage(page + 1)} />}
-        {page === 1 && <Questioner onClick={() => setPage(page + 1)} />}
+        {/* {page === 0 && <WarnModal onClick={() => setPage(page + 1)} />} */}
+        {/* {page === 1 && <Questioner onClick={() => setPage(page + 1)} />} */}
       </main>
       <main>
-        {page === 2 && <PickEventModule onClick={() => setPage(page + 1)} />}
-        {page === 3 && <AfterRegistrationModule />}
+        {page === 0 && <PickEventModule onClick={() => setPage(page + 1)} />}
+        {page === 1 && <AfterRegistrationModule />}
       </main>
     </>
   )
